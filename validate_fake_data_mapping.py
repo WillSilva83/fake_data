d0459bf9dfe887041348f1d1a45575989af2a22f
William de Souza <williamssouza89@gmail.com>
2025-04-13 20:24:51 -0300

    Ajustes de funcionalidades - Código funcional :)

--- validate_fake_data_mapping.py
+++ validate_fake_data_mapping.py
@@ -1,99 +1,123 @@
 from libs import fake_data_with_mapping  as fake_wm
 from libs import fake_data as fd
-from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, DoubleType, BooleanType, DateType, TimestampType
 import random
-import re 
 from faker import Faker 
+from datetime import datetime
 
-spark = SparkSession.builder.appName("GenerateDataFrameFake").getOrCreate() 
 faker = Faker() 
 
 # TO-DOs 
 
-# Numeros apenas negativos 
-# Numeros com 2 casas decimais 
-# Formatacao com zeros a direita
-# 
+# 1 - Numeros apenas negativos - OK 
+# 2 - Numeros com 2 casas decimais - OK 
+# 3 - Formatacao com zeros a direita -  
+# 4 - Campos derivados de outros campos 
+# 5 - Ajustar para ler o JSON e converter os valores corretos 
+# 6 - Campos do tipo decimal 
 
-def generate_formatted_data(format_string, num_rows):
-    pattern = re.compile(format_string)
-    if format_string == "####-##-##":
-        return [faker.date_between(start_date="-1y", end_date="today").strftime("%Y-%m-%d") for _ in range(num_rows)]
+
+def generate_data_for_field(field_name: str, field_type: str, field_config, row, index: int):
+    """ Gera os dados com base na configuração do campo"""
+
+    def generate_numeric(field_config, field_type):
+        is_negative = field_config.get("is_negative_number", "False") == "True"
+        
+        if field_type in ["double", "float"]:
+            precision = int(field_config.get("round", 2))
+            base = random.uniform(-1000, -1) if is_negative else random.uniform(0, 1000)
+            return round(base, precision)
+        
+        elif field_type == "bigint":
+            return random.randint(-1000, -1) if is_negative else random.randint(0, 1000)
+        
+        else:
+            raise ValueError(f"Unsupported numeric type: {field_type}")
+
+    if field_config.get("is_sequencial", "False") == "True":
+        return index + 1  # Sequencial gerado com base no índice
+    
+    elif field_config.get("is_numeric", "False") == "True":
+        return generate_numeric(field_config, field_config.get("type", "bigint"))
+    
+    elif field_config.get("is_derived", "False") == "True":
+        derived_field = field_config["derived_field"]
+        derived_size = int(field_config.get("derived_size", 10))
+        return str(row.get(derived_field, ""))[:derived_size]
+
+    elif "format" in field_config:
+        # Gerar dados com bothify para formatos
+        if field_config["format"] == "####-##-##":
+            return faker.date_between(start_date="-1y", end_date="today").strftime("%Y-%m-%d")
+        else:
+            return faker.bothify(field_config["format"])
+    
+    elif field_type == "boolean":
+        return random.choice([True, False])
+    
+    elif field_type == "date":
+        date_str = faker.date_between(start_date="-1y", end_date="today").strftime("%Y-%m-%d")
+        return datetime.strptime(date_str, "%Y-%m-%d").date()
+    
+    elif field_type == "timestamp":
+        timestamp_str = faker.date_time().isoformat()
+        return datetime.fromisoformat(timestamp_str)
+    
+    elif "choise" in field_config:
+        return random.choice(field_config["choise"])
+    
+    elif "default" in field_config:
+        return field_config["default"]
+    
+    elif field_type == "string":
+        return f"random_{random.randint(1000, 9999)}"
     else:
-        return [faker.bothify(format_string) for _ in range(num_rows)]
-
-
+        return None
 
 def generate_dataframe_with_mapping(aws_table_fields, config, table_name, num_rows):
-    
-    table_config = config.get(table_name, {})
+    """ Gera um Dataframe com base no mapeamento """
+    from pyspark.sql import SparkSession
+    from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, FloatType, BooleanType, DateType, TimestampType
 
-    def generate_column_data(field_name, field_type):
-        if field_name in table_config:
-            field_config = table_config[field_name]
+    spark = SparkSession.builder.appName("GenerateDataFrameFake").getOrCreate() 
 
-            if "is_sequencial" in field_config:
-                return list(range(1, num_rows+1))
-            elif "choise" in field_config:
-                return [random.choice(field_config["choise"]) for _ in range(num_rows)]
-            elif "default" in field_config: 
-                return [field_config["default"]] * num_rows
-            elif "format" in field_config:
-                return generate_formatted_data(field_config["format"], num_rows)
-            elif "round" in field_config:
-                return 
+    field_types = {
+        "bigint": IntegerType(),
+        "string": StringType(),
+        "double": DoubleType(),
+        "float": FloatType(),
+        "boolean": BooleanType(),
+        "date": DateType(),
+        "timestamp": TimestampType()
+    }
 
-        else:
-            if field_type == "bigint":
-                return [random.randint(0, 1000) for _ in range(num_rows)]
-            elif field_type == "string": 
-                return [f"random_{random.randint(1000, 9999)}" for _ in range(num_rows)]
-            elif field_type == "double":
-                return [random.uniform(1.0, 100.0) for _ in range(num_rows)]
-            elif field_type == "float":
-                return [random.uniform(1.0, 100.0) for _ in range(num_rows)]
-            elif field_type == "boolean":
-                return [random.choice([True, False]) for _ in range(num_rows)]
-            elif field_type == "date":
-                return [faker.date_object() for _ in range(num_rows)]
-            elif field_type == "timestamp":
-                return [faker.date_time() for _ in range(num_rows)]
 
-            else:
-                return [None] * num_rows 
-    
-    data = {}
+    config_table = config.get(table_name, {})
+    rows = []
 
-    for field in aws_table_fields:
-        field_name = field["Name"]
-        field_type = field["Type"]
-        
-        data[field_name] = generate_column_data(field_name, field_type)
+    for i in range(num_rows):
+        row = {}
+        for field in aws_table_fields:
+            field_name = field['Name']
+            field_type = field['Type']
+            field_config = config_table.get(field_name, {})
+            row[field_name] = generate_data_for_field(
+                field_name, field_type, field_config, row, i
+            )
+        rows.append(row)
 
     schema = StructType([
-        StructField(field["Name"], 
-                    IntegerType() if field["Type"] == "bigint" else 
-                    DoubleType() if field["Type"] == "double" else 
-                    FloatType() if field["Type"] == "float" else 
-                    BooleanType() if field["Type"] == "boolean" else 
-                    DateType() if field["Type"] == "date" else 
-                    TimestampType() if field["Type"] == "timestamp" else 
-                    StringType(), 
-                    True)
+        StructField(field['Name'], field_types.get(field['Type'], StringType()), True)
         for field in aws_table_fields
     ])
-
-    rows = list(zip(*data.values()))
 
     return spark.createDataFrame(rows, schema)
 
 
 
-#file_mapping = "table1_test.json"
-#table_name = "table1_test"
-file_mapping = "table_test_2.json"
-table_name = "table_test_2"
+file_mapping = "table1_test.json"
+table_name = "table1_test"
+#file_mapping = "table_test_2.json"
+#table_name = "table_test_2"
 
 database_name = "database_test"
 num_rows = 10 
@@ -104,7 +128,6 @@ config = fake_wm.read_mapping_json(file_mapping)
 table_metadata = fd.get_table(database_name, table_name)
 
 aws_table_fields = fd.treatment_columns(table_metadata)
-
 
 df = generate_dataframe_with_mapping(aws_table_fields, config, table_name, num_rows)
 
