41456128d783fa1dace7b322109bb6a895f7d8c9
William de Souza <williamssouza89@gmail.com>
2025-04-17 09:16:39 -0300

    Atualizacao de logica

--- main.py
+++ main.py
@@ -51,9 +51,9 @@ if __name__ == "__main__":
     partition_by = ""
     overwrite_partition = sys.argv[3] # Obrigatorio
 
-    config_file = sys.argv[3] if len(sys.argv) > 3 else None            # Opcional 
-    num_rows = sys.argv[4] if len(sys.argv) > 4    else 10              # Opcional
-    process_date = sys.arg[5] if len(sys.argv) > 5 else dia_anterior    # Opcional
+    config_file = sys.argv[4] if len(sys.argv) > 4 else None            # Opcional 
+    num_rows = sys.argv[5] if len(sys.argv) > 5    else 10              # Opcional
+    process_date = sys.arg[6] if len(sys.argv) > 6 else dia_anterior    # Opcional
 
    
     main(table_name, database_name, config_file, num_rows, spark)
